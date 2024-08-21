5e9a512300791692e8df63000cc9f501f3ed45ae
Balazs Hocsak <bhocsak@c4r7p10.42vienna.com>
2024-08-21 11:26:25 +0200

    price calculation upgraded

--- implementation_utils.c
+++ implementation_utils.c
@@ -12,20 +12,26 @@
 
 #include "push_swap.h"
 
-static void	all_price_tagging(int len_a, t_stack **b)
+static void	all_price_tagging(t_stack **a, t_stack **b)
 {
+	t_stack	*cur_a;
 	t_stack	*cur_b;
 
+	cur_a = *a;
 	cur_b = *b;
+	while (cur_a)
+	{
+		cur_a->price = cur_a->position;
+		if (!cur_a->above_median)
+			cur_a->price = list_length(a) - cur_a->position;
+		cur_a = cur_a->next;
+	}
 	while (cur_b)
 	{
 		cur_b->price = cur_b->position;
-		if (cur_b->above_median == 0)
-			cur_b->price = list_length(b) - (cur_b->position);
-		if (cur_b->target->above_median)
-			cur_b->price = cur_b->position + cur_b->target->position;
-		else
-			cur_b->price = cur_b->position + len_a - (cur_b->target->position);
+		if (!cur_b->above_median)
+			cur_b->price = list_length(b) - cur_b->position;
+		cur_b->price += cur_b->target->price;
 		cur_b = cur_b->next;
 	}
 }
@@ -92,7 +98,7 @@ void	setting_cheapest(t_stack **a, t_stack **b)
 	cur_a = *a;
 	cur_b = *b;
 	best_price = INT_MAX;
-	all_price_tagging(list_length(a), b);
+	all_price_tagging(a, b);
 	while (cur_b)
 	{
 		if (cur_b->price < best_price)
